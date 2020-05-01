fd70ca823b5d82ca5e8fc4f67228a4aad7a3c8ff
anagha <anagharaj95@gmail.com>
2020-05-01 20:25:47 +0530

    Added bootstrap and works showing up

--- works/views.py
+++ works/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Work
 
-# Create your views here.
+def home(request):
+    works = Work.objects
+    return render(request, 'works/home.html', {'works':works})
