e87cd2525d33844b0d1da62bf8bdc68bb5a272a7
anagha <anagharaj95@gmail.com>
2020-05-03 19:34:31 +0530

    website completed - final commit

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,12 @@ class Blog(models.Model):
     publication_date = models.DateTimeField()
     content = models.TextField()
     image = models.ImageField(upload_to='images/')
+    
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.content[:100]
+    
+    def pub_date_pretty(self):
+        return self.publication_date.strftime('%b %e %Y')
